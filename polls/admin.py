70f347706a83293470362554a50ffa7ac6226790
Arion2000 <github@arion2000.xyz>
2025-02-01 15:08:08 +0100

    Add choices in admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 0
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date')
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
