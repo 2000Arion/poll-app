bd9a30a20d65539a19e57c20eb2bfe6027b34a65
Arion2000 <github@arion2000.xyz>
2025-02-01 15:42:48 +0100

    Update admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -22,8 +22,12 @@ class ChoiceInline(admin.TabularInline):
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('question_text', 'pub_date')
+    list_display = ('question_text', 'total_votes', 'pub_date')
     inlines = [ChoiceInline]
+
+    def total_votes(self, obj):
+        return sum(choice.votes for choice in obj.choice_set.all())
+    total_votes.short_description = "Gesamtstimmen"
 
     def save_model(self, request, obj, form, change):
         super().save_model(request, obj, form, change)
